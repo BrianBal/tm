078ee8123c01daae3e2d84403f6dfd09704c3954
Brian Bal <briantbal@gmail.com>
2014-06-22 19:35:20 -0700

    Added emailer and testflight skelton plugins

---
+++ plugins/emailer/emailer_plugin.rb
@@ -0,0 +1,13 @@
+class EmailerPlugin < Plugin
+
+  def plugin_tag
+    "emailer"
+  end
+
+  def after_test(project)
+  end
+
+  def after_build(project)
+  end
+
+end
---
+++ plugins/testflight/testflight_plugin.rb
@@ -0,0 +1,13 @@
+class TestFlightPlugin < Plugin
+
+  def plugin_tag
+    "testflight"
+  end
+
+  def after_test(project)
+  end
+
+  def after_build(project)
+  end
+
+end
